c53728fe7d24d2844a72b787a865f825fd94626f
swapman972 <jordan.melidor@hotmail.fr>
2020-06-15 17:05:58 -0400

    submit and redirect

--- src/components/signUpQuizFolder/SignUp.js
+++ src/components/signUpQuizFolder/SignUp.js
@@ -5,7 +5,7 @@ export default class SignUp extends React.Component{
     render(){
         return(
             <div>
-                You Are all signed up! should redirect to topic/post now
+                You Are all signed up! should be redirect to topic/post now
             </div>
         )
     }
