8c92d5e21d8f7a2905cc4958656b3b1980a759bf
theluispestana <theluispestana@gmail.com>
2020-06-16 12:00:17 -0400

    added headers to request file

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
 // headers --> use these at your own discretion
-// const headers = {'Content-Type': 'application/json', 'Accepts': 'application/json'}
-// urls
+// const headers = {'Content-Type': 'application/json', 'Accepts': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}`}
+// // urls
 const baseUrl = "http://localhost:3000";
 const topicsUrl = `${baseUrl}/topics/`;
 
@@ -12,4 +12,5 @@ const catchError = (error) => console.log(`%c${error}`, "color: red;");
 // fetchs all topics with associated comments
 // return promise
 export const fetchTopics = () =>
-  fetch(topicsUrl).then(parseData).catch(catchError);
+  fetch(topicsUrl, headers).then(parseData).catch(catchError);
+
