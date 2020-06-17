f681070f0969f1c9e36ee120232c105fa2cc1e4c
theluispestana <theluispestana@gmail.com>
2020-06-17 11:03:53 -0400

    fixed token validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,9 +19,9 @@ class LoginForm extends React.Component {
     requests.loginUser(this.state).then((json) => {
       console.log(json);
       localStorage.setItem("token", json.jwt);
-      localStorage.setItem("userId", json.user.id)
-      localStorage.setItem("userName", json.user.name)
-      localStorage.setItem("userGroup", json.user.group_id)
+      localStorage.setItem("userId", json.user.id);
+      localStorage.setItem("userName", json.user.name);
+      localStorage.setItem("userGroup", json.user.group_id);
       this.setState(this.state);
     });
   };
@@ -29,7 +29,7 @@ class LoginForm extends React.Component {
   render() {
     return (
       <div>
-        {localStorage.token !== "null" ? <Redirect to="/home" /> : null}
+        {localStorage.getItem("token") ? <Redirect to="/home" /> : null}
         <form onChange={this.handleChange}>
           <input type="text" name="name" placeholder="Username" />
           <input type="password" name="password" placeholder="password" />
